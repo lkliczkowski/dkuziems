baf9a08cf8e366e0fbf9e1334fa9de94f477990b
nightal87 <nightal87@9402d4cc-df25-371b-9c69-222f02b41e37>
2011-05-06 21:31:25 +0000

    Algorytm nauczania sieci Levenberga-Marquardta z poprawionym systemem obliczeń zaproponowanym przez B.M.Wilamowskiego. Wszystko chyba ze strony mechanicznej działa, pozostały kwestie "kosmetyczne".

---
+++ improvedLM/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImprovedLM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TESTOWE
+            int hiddenNodeRatio = 6;
+            ulong maxEpochs = 1500;
+            double desiredAcc = 100;
+
+            string inputFile = "letter-a-recognition.csv";
+            //string inputFile = "HeartDisease.csv";
+            //ZScore.EnumDataTypes dataType = ZScore.EnumDataTypes.HeartDisease;
+            ZScore.EnumDataTypes dataType = ZScore.EnumDataTypes.LetterRecognitionA;
+            Console.WriteLine("Przygotowywanie danych");
+            ZScore.ZScore Dataset = new ZScore.ZScore(inputFile, dataType);
+            int hiddenNodeRatioPar;
+            if (Dataset.NormalizeRun())
+            {
+                Console.WriteLine("Standaryzacja Z-Score zakończona z powodzeniem!");
+                hiddenNodeRatioPar = Dataset.sample(0).Length - 1;
+
+                ImprovedLMTrainer nn = new ImprovedLMTrainer(Dataset, hiddenNodeRatio, maxEpochs, desiredAcc);
+                if (nn.trainNetwork())
+                {
+                    Console.WriteLine("ZAKOŃCZONO TRENOWANIE SIECI NEURONOWEJ!");
+                    nn.PrintStatus();
+                }
+                else
+                {
+                    Console.WriteLine("ZAKOŃCZONO TRENOWANIE SIECI NEURONOWEJ! (NIEPOWODZENIE)");
+                    nn.PrintStatus();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("StandaryzacjaZ (ZScore) nie powiodła się!");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void PrintLongLine()
+        {
+            Console.WriteLine("============================================================================");
+        }
+    }
+}
---
+++ improvedLM/ZScoreEnumDataTypes.cs
@@ -0,0 +1,50 @@
+﻿﻿
+namespace ZScore
+{
+    public enum EnumDataTypes
+    {
+        unknown = 0,
+        HeartDisease = 1,
+        LetterRecognitionA = 2
+    }
+    public enum EnumHeartDisease
+    {
+        Value = 0, //continuous
+        LowMediumHigh = 1,
+        AbsentPresent = 2,
+        Obesity = 3,
+        AgeRange = 4
+
+    }
+    public enum EnumLowMediumHigh
+    {
+        unknown = -1,
+        low = 0,
+        medium = 1,
+        high = 2
+    }
+
+    public enum EnumAbsentPresent
+    {
+        unknown = -1,
+        Absent = 0,
+        Present = 1
+    }
+
+    public enum EnumObesity
+    {
+        unknown = -1,
+        underweight = 0,
+        Healthy = 1,
+        overweight = 2,
+        clinicallyobese = 3
+    }
+
+    public enum EnumAgeRange
+    {
+        unknown = -1,
+        young = 0,
+        middleaged = 1,
+        old = 2,
+    }
+}
